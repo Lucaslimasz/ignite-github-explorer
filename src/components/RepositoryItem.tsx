a0b37673ff5372b2d3a4ae951d34de9fb53370a4
Lucas Lima <lucaslima@Lucass-MacBook-Air.local>
2021-11-06 17:01:49 -0300

    list and item components, typescript added

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,22 @@
+interface RepositoryItem {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+function RepositoryItem({repository}: RepositoryItem) {
+  return (
+    <li>
+      <strong>{repository.name ?? 'Default'}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.html_url}>
+        Acessar repositório
+      </a>
+    </li>
+  )
+}
+
+export default RepositoryItem;
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import RepositoryItem from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/lucaslimasz/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {
+          repositories.map(repository => (
+            <RepositoryItem key={repository.name} repository={repository} />
+          ))
+        }
+      </ul>
+    </section>
+  );
+}
+
+export default RepositoryList;
